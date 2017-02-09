befc6e5793f59dabdf58c394e805d93bada96728
Alex Hartle <alex@jump2go.com>
2017-02-09 13:22:04 -0800

    added early game logic

--- deckOfCards/Game.cs
+++ deckOfCards/Game.cs
@@ -1,3 +1,21 @@
-namespace DeckOfCards {
+namespace BlackJack {
+    public class Gameplay {
+        public static string Action(string act) {
+            string result = "nothing";
+            if (act == "hit") {
+                System.Console.WriteLine("Player Hit!");
+                result = "Hit";
+            }
+            if (act == "stay") {
+                System.Console.WriteLine("Player Stayed.");
+                result = "Stay";
+            }
+            else {
+                System.Console.WriteLine("Player input nonsense.");
+                result = "I don't understand. You can 'hit' or 'pass'.";
+            }
+            return result;
+        }
+    }
     
 }
